1bfb9be4c98e25c17f1963755a7de46cb7d685e6
Aloke Desai <aloke.desai@gmail.com>
2014-07-26 20:35:48 -0700

    add slideshare html if url is from slideshare

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -1,4 +1,11 @@
 class Resource < ActiveRecord::Base
   validate :title, presence: true
   validate :url, presence: true
+  before_save :set_slideshare_html
+
+  def set_slideshare_html
+    return unless self.url.include? "slideshare"
+    response = HTTParty.get("http://www.slideshare.net/api/oembed/2?url=#{self.url}&format=json")
+    self.slideshare_html = response["html"] if response["html"].present?
+  end
 end
