22f282fdbcf7e651002da1ad53f7fa3406e51af0
Aloke Desai <aloke.desai@gmail.com>
2014-07-27 16:40:42 -0700

    init

--- app/controllers/recruiters_controller.rb
+++ app/controllers/recruiters_controller.rb
@@ -1,2 +1,21 @@
 class RecruitersController < ApplicationController
+  def new
+    @recruiter = Recruiter.new
+  end
+
+  def create
+    recruiter = Recruiter.new alum_params
+    if recruiter.valid?
+      recruiter.save
+      flash[:notice] = "Thank you for your interest in SCM. A message was sent to the SCM leadership. They will contact you shortly."
+    else
+      flash[:error] = recruiter.errors.full_messages.join(", ")
+    end
+    redirect_to :back
+  end
+
+  private
+  def alum_params
+    params.require(:recruiter).permit(:name, :email, :company)
+  end
 end
