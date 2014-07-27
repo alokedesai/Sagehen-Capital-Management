2ba8ca1b2c84819527b16bf0c943d066fb1a5383
Aloke Desai <aloke.desai@gmail.com>
2014-07-26 21:34:02 -0700

    recruiter init

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140727030040) do
+ActiveRecord::Schema.define(version: 20140727042659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20140727030040) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+  end
+
+  create_table "recruiters", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "company"
+    t.string   "interest"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "resources", force: true do |t|
