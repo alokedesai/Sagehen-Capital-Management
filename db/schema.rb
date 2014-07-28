1d158b5cb5bd66787d3d1e287c0dec2712aa1229
Aloke Desai <aloke.desai@gmail.com>
2014-07-27 17:25:15 -0700

    add user_id to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140727233832) do
+ActiveRecord::Schema.define(version: 20140728000900) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20140727233832) do
     t.string   "ticker"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "recruiters", force: true do |t|
