422d348fb609a8135d3c7e65e46839e82ac742b8
Aloke Desai <aloke.desai@gmail.com>
2014-08-03 16:51:51 -0700

    remove model that also should have been a migration

--- app/models/remove_attributes_from_presentation.rb
+++
@@ -1,6 +0,0 @@
-class RemoveAttributesFromPresentation < ActiveRecord::Base
-  remove_column :presentations, :ticker
-  remove_column :presentations, :action
-  remove_column :presentations, :thesis
-  remove_column :presentations, :slideshare_url
-end
--- db/migrate/20140730044237_create_remove_attributes_from_presentations.rb
+++
@@ -1,8 +0,0 @@
-class CreateRemoveAttributesFromPresentations < ActiveRecord::Migration
-  def change
-    create_table :remove_attributes_from_presentations do |t|
-
-      t.timestamps
-    end
-  end
-end
