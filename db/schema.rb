9efed1fbf08ccd4f650cf7a8a6b3864de871b360
Aloke Desai <aloke.desai@gmail.com>
2014-08-04 21:32:00 -0700

    init

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140803023551) do
+ActiveRecord::Schema.define(version: 20140805042135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 20140803023551) do
     t.string   "name"
     t.string   "position_type"
     t.integer  "position_order"
+    t.boolean  "current_management", default: true
   end
 
   create_table "ckeditor_assets", force: true do |t|
@@ -51,6 +52,13 @@ ActiveRecord::Schema.define(version: 20140803023551) do
 
   add_index "ckeditor_assets", ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable", using: :btree
   add_index "ckeditor_assets", ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type", using: :btree
+
+  create_table "current_managements", force: true do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", force: true do |t|
     t.string   "title"
