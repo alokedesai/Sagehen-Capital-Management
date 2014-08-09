0f982c17b3f9d6a5114487a3d10f5ba506853034
Aloke Desai <aloke.desai@gmail.com>
2014-08-08 23:09:56 -0700

    show helpful message if a user has an account but isnt' approved

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,8 +1,16 @@
 class Admin::AdminController < ApplicationController
   before_filter :verify_admin
+  before_filter :verify_approved
 
   private
   def verify_admin
-    redirect_to root_url unless (current_user && current_user.approved)
+    redirect_to root_url unless current_user
+  end
+
+  def verify_approved
+    unless current_user.approved
+      flash[:error] = "An admin has not approved you to access the backend yet."
+      redirect_to root_url
+    end
   end
 end
