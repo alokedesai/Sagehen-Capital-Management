245a8eec28dd7b94ce750256a2ec761c6e95ac0c
Aloke Desai <aloke.desai@gmail.com>
2014-08-17 21:47:37 -0400

    add executive column to alums

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811032802) do
+ActiveRecord::Schema.define(version: 20140818014643) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20140811032802) do
     t.string   "position_type"
     t.integer  "position_order"
     t.boolean  "current_management", default: true
+    t.boolean  "executive",          default: true
   end
 
   create_table "ckeditor_assets", force: true do |t|
