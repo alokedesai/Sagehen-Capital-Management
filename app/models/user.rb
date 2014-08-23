bd64d8783d919afec6c63c1fc60050eeb6a2db03
Aloke Desai <aloke.desai@gmail.com>
2014-08-22 23:32:20 -0400

    fix 500 error that occurs if a user tries to signup without a school associated to their linkedin account

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,13 @@ class User < ActiveRecord::Base
     user.linkedin_id = auth["extra"]["raw_info"]["id"] 
     user.image_url = auth["info"]["image"]
     user.linkedin_url = auth["info"]["urls"]["public_profile"]
-    auth["extra"]["raw_info"]["educations"]["values"].each do |school|
-      if "Pomona".in? school["schoolName"]
-        user.major = school["fieldOfStudy"]
-        user.grad_year = school["endDate"]["year"]
+    education = auth["extra"]["raw_info"]["educations"]
+    if education
+      education["values"].each do |school|
+        if "Pomona".in? school["schoolName"]
+          user.major = school["fieldOfStudy"]
+          user.grad_year = school["endDate"]["year"]
+        end
       end
     end
     user.save!
