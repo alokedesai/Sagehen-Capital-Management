e532e6cfd6df700e8f38d8356b558cc089976bc6
Aloke Desai <aloke.desai@gmail.com>
2014-12-20 22:22:27 -0500

    Query from mongolab data to get portfolio data

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -32,32 +32,14 @@ class StocksController < ApplicationController
   end
 
   def portfolio_data
-    # get the prices for each ticker
-    portfolio_prices = []
-    Stock.pluck(:ticker).each do |ticker|
-      begin
-        portfolio_prices << StockQuote::Stock.json_quote(ticker, 3.months.ago, Date.today, ['Date', 'Close', 'Symbol'])
+    uri = "mongodb://aloke:sagehen@ds027771.mongolab.com:27771/stocks"
+    client = Mongo::MongoClient.from_uri(uri)
+    db_name = uri[%r{/([^/\?]+)(\?|$)}, 1]
+    db = client.db(db_name)
+    stocks = db.collection("portfolio")
 
-      # silently fail if the ticker can't be found
-      rescue StockQuote::NoDataForStockError
-        next
-      end
-    end
-
-    # aggregate the data by summing the price weighted by the # of shares
-    result = []
-    portfolio_prices.each do |stock_info|
-      num_shares = Stock.find_by_ticker(stock_info["quote"].first["Symbol"].upcase).share_count
-      stock_info["quote"].each_with_index do |data, i|
-        if result[i]
-          result[i][1] += num_shares * data["Close"].to_i
-        else
-          result[i] = [data["Date"].to_date.strftime("%Q").to_i, num_shares * data["Close"].to_i]
-        end
-      end
-    end
     render :json => {
-      :quote => result.reverse!
+      :quote => stocks.find_one["quote"]
     }
   end
 
