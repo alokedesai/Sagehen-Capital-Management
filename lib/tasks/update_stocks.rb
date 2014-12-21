81b6aee9b4e005b390733da986f2c025f16514dd
Aloke Desai <aloke.desai@gmail.com>
2014-12-20 22:10:53 -0500

    init; file for cron job that will update stocks

---
+++ lib/tasks/update_stocks.rb
@@ -0,0 +1,47 @@
+require "mongo"
+require "json"
+
+
+portfolio_prices = []
+Stock.pluck(:ticker).each do |ticker|
+  begin
+    portfolio_prices << StockQuote::Stock.json_quote(ticker, 3.months.ago, Date.today, ['Date', 'Close', 'Symbol'])
+
+  # silently fail if the ticker can't be found
+  rescue StockQuote::NoDataForStockError
+    next
+  end
+end
+
+# aggregate the data by summing the price weighted by the # of shares
+result = []
+portfolio_prices.each do |stock_info|
+  num_shares = Stock.find_by_ticker(stock_info["quote"].first["Symbol"].upcase).share_count
+  stock_info["quote"].each_with_index do |data, i|
+    if result[i]
+      result[i][1] += num_shares * data["Close"].to_i
+    else
+      result[i] = [data["Date"].to_date.strftime("%Q").to_i, num_shares * data["Close"].to_i]
+    end
+  end
+end
+
+# update mongolab
+data = {:quote => result.reverse!}
+
+uri = "mongodb://aloke:sagehen@ds027771.mongolab.com:27771/stocks"
+
+client = Mongo::MongoClient.from_uri(uri)
+
+db_name = uri[%r{/([^/\?]+)(\?|$)}, 1]
+
+db = client.db(db_name)
+
+# First we'll add a few songs. Nothing is required to create the songs 
+# collection; it is created automatically when we insert.
+
+stocks = db.collection("portfolio")
+
+stocks.remove
+stocks.insert data
+
